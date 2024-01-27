2dfc3031654d03013fff1c879a629058e38a921c
nathankoerschner <nathankoerschner@gmail.com>
2024-01-27 16:32:59 -0500

    Use app.config

---
+++ app.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+	expo: {
+		name: "tibetan-notes-app",
+		slug: "tibetan-notes-app",
+		version: "1.0.0",
+		orientation: "portrait",
+		icon: "./assets/icon.png",
+		userInterfaceStyle: "light",
+		splash: {
+			image: "./assets/splash.png",
+			resizeMode: "contain",
+			backgroundColor: "#ffffff",
+		},
+		assetBundlePatterns: ["**/*"],
+		ios: {
+			supportsTablet: true,
+			bundleIdentifier: "com.nathankoerschner.tibetannotesapp",
+			googleServicesFile:
+				process.env.GOOGLE_SERVICE_INFO_PLIST ?? "./GoogleService-Info.plist",
+		},
+		android: {
+			googleServicesFile:
+				process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+			adaptiveIcon: {
+				foregroundImage: "./assets/adaptive-icon.png",
+				backgroundColor: "#ffffff",
+			},
+			package: "com.nathankoerschner.tibetannotesapp",
+		},
+		web: {
+			favicon: "./assets/favicon.png",
+		},
+		plugins: [
+			"@react-native-firebase/app",
+			[
+				"expo-build-properties",
+				{
+					ios: {
+						useFrameworks: "static",
+					},
+				},
+			],
+		],
+		extra: {
+			eas: {
+				projectId: "40eb841b-0961-418b-a478-1a935869dce4",
+			},
+		},
+		owner: "nathankoerschner",
+	},
+};
